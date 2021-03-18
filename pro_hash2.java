e09073eeb79c72f6a5f4ab0722789b0714ec0b5c
YoonHeeMoon <yhm721@naver.com>
2021-03-18 21:58:34 +0900

    add pro_hash2

---
+++ pro_hash2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class pro_hash2 {
+    public static void main(String[] args) {
+        String[] phone_book = { "1", "234", "543", "567","577","587","556","56778", "8" };
+
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length -1; i++) {
+             if(phone_book[i+1].indexOf(phone_book[i]) == 0){
+                 System.out.println(true);
+             }
+        }
+
+
+      
+
+    }
+}
